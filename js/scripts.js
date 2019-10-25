6c7cdbcbcf60d445d2ffcd409b8b4f8c3f0ae488
Dylan Manley <dylan.manley@gmail.com>
2019-10-25 12:57:26 -0700

    add css styling and classification to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
 $( document ).ready(function() {
-  $("input#customRadio1").click(function(){
-    $("#javascript").show();
-    $("#ruby").hide();
-    $("#react").hide();
-    $("#html").hide();
-  });
-  $("input#customRadio2").click(function(){
-    $("#ruby").show();
-    $("#javascript").hide();
-    $("#react").hide();
-    $("#html").hide();
-  });
-  $("input#customRadio3").click(function(){
-    $("#react").show();
-    $("#javascript").hide();
-    $("#ruby").hide();
-    $("#html").hide();
-  });
-  $("input#customRadio4").click(function(){
-    $("#html").show();
-    $("#javascript").hide();
-    $("#ruby").hide();
-    $("#react").hide();
-  });
+  var number1 = parseInt()
+  var number2 = parseInt()
+  var number3 = parseInt()
+  var number4 = parseInt()
+  var number5 = parseInt()
+
+  // $("input#customRadio1").click(function(){
+  //   $("#javascript").show();
+  //   $("#ruby").hide();
+  //   $("#react").hide();
+  //   $("#html").hide();
+  // });
+  // $("input#customRadio2").click(function(){
+  //   $("#ruby").show();
+  //   $("#javascript").hide();
+  //   $("#react").hide();
+  //   $("#html").hide();
+  // });
+  // $("input#customRadio3").click(function(){
+  //   $("#react").show();
+  //   $("#javascript").hide();
+  //   $("#ruby").hide();
+  //   $("#html").hide();
+  // });
+  // $("input#customRadio4").click(function(){
+  //   $("#html").show();
+  //   $("#javascript").hide();
+  //   $("#ruby").hide();
+  //   $("#react").hide();
+  // });
     console.log( "ready!" );
 });
