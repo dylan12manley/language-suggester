47bfdf40f8a7608f0588aadff908d692e565729b
Dylan Manley <dylan.manley@gmail.com>
2019-10-25 14:53:22 -0700

    add functionality to js, comment out the radio form

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,64 @@
 $( document ).ready(function() {
   $("form#suggester").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("option#experience").val());
+    var number1 = parseInt($("select#experience").val());
+    var number2 = parseInt($("select#frontOrBack").val());
+    var number3 = parseInt($("select#jobOrSkill").val());
+    var number4 = parseInt($("select#learn").val());
+    var number5 = parseInt($("select#nerd").val());
     console.log("number1", number1)
+    if (number1 === 1){
+      $("#html").show();
+      $("#javascript").hide();
+      $("#react").hide();
+      $("#ruby").hide();
+    }
+    else if (number1 === 10){
+      $("#javascript").show();
+      $("#html").hide();
+      $("#react").hide();
+      $("#ruby").hide();
+    }
+    else if (number1 === 100){
+      $("#javascript").hide();
+      $("#html").hide();
+      $("#react").hide();
+      $("#ruby").show();
+    }
+    else {
+      $("#react").show();
+      $("#javascript").hide();
+      $("#html").hide();
+      $("#ruby").hide();
+    }
   });
-
-  // var sel = document.getElementById('scripts');
-
-  // display value property of select list (from selected option)
-  // console.log( sel.value );
-  // $("select#experience").submit(function(event) {
-  //   event.preventDefault();
-  // })
 
 // This is the script for the #questionReal//
 
-  $("input#customRadio1").click(function(){
-    $("#javascript").show();
-    $("#ruby").hide();
-    $("#react").hide();
-    $("#html").hide();
-  });
-  $("input#customRadio2").click(function(){
-    $("#ruby").show();
-    $("#javascript").hide();
-    $("#react").hide();
-    $("#html").hide();
-  });
-  $("input#customRadio3").click(function(){
-    $("#react").show();
-    $("#javascript").hide();
-    $("#ruby").hide();
-    $("#html").hide();
-  });
-  $("input#customRadio4").click(function(){
-    $("#html").show();
-    $("#javascript").hide();
-    $("#ruby").hide();
-    $("#react").hide();
-  });
+  // $("input#customRadio1").click(function(){
+  //   $("#javascript").show();
+  //   $("#ruby").hide();
+  //   $("#react").hide();
+  //   $("#html").hide();
+  // });
+  // $("input#customRadio2").click(function(){
+  //   $("#ruby").show();
+  //   $("#javascript").hide();
+  //   $("#react").hide();
+  //   $("#html").hide();
+  // });
+  // $("input#customRadio3").click(function(){
+  //   $("#react").show();
+  //   $("#javascript").hide();
+  //   $("#ruby").hide();
+  //   $("#html").hide();
+  // });
+  // $("input#customRadio4").click(function(){
+  //   $("#html").show();
+  //   $("#javascript").hide();
+  //   $("#ruby").hide();
+  //   $("#react").hide();
+  // });
 
     console.log( "ready!" );
 });
