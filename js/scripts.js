2e603d878a01756422131de3ff7a6633eb9cacd9
Dylan Manley <dylan.manley@gmail.com>
2019-10-25 13:32:55 -0700

    add background in css, broken and commented out functions in js

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,40 @@ $( document ).ready(function() {
   var number4 = parseInt()
   var number5 = parseInt()
 
-  // $("input#customRadio1").click(function(){
-  //   $("#javascript").show();
-  //   $("#ruby").hide();
-  //   $("#react").hide();
-  //   $("#html").hide();
-  // });
-  // $("input#customRadio2").click(function(){
-  //   $("#ruby").show();
-  //   $("#javascript").hide();
-  //   $("#react").hide();
-  //   $("#html").hide();
-  // });
-  // $("input#customRadio3").click(function(){
-  //   $("#react").show();
-  //   $("#javascript").hide();
-  //   $("#ruby").hide();
-  //   $("#html").hide();
-  // });
-  // $("input#customRadio4").click(function(){
-  //   $("#html").show();
-  //   $("#javascript").hide();
-  //   $("#ruby").hide();
-  //   $("#react").hide();
-  // });
+  var sel = document.getElementById('scripts');
+
+  // display value property of select list (from selected option)
+  // console.log( sel.value );
+  // $("select#experience").submit(function(event) {
+  //   event.preventDefault();
+  // })
+
+// This is the script for the #questionReal//
+
+  $("input#customRadio1").click(function(){
+    $("#javascript").show();
+    $("#ruby").hide();
+    $("#react").hide();
+    $("#html").hide();
+  });
+  $("input#customRadio2").click(function(){
+    $("#ruby").show();
+    $("#javascript").hide();
+    $("#react").hide();
+    $("#html").hide();
+  });
+  $("input#customRadio3").click(function(){
+    $("#react").show();
+    $("#javascript").hide();
+    $("#ruby").hide();
+    $("#html").hide();
+  });
+  $("input#customRadio4").click(function(){
+    $("#html").show();
+    $("#javascript").hide();
+    $("#ruby").hide();
+    $("#react").hide();
+  });
+
     console.log( "ready!" );
 });
