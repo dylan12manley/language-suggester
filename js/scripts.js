e65e2661d023cbf1762403ea490c0bf43b6facd8
Dylan Manley <dylan.manley@gmail.com>
2019-10-25 12:06:42 -0700

    add question a select form to html, html class in css and functionality to js

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,25 @@ $( document ).ready(function() {
     $("#javascript").show();
     $("#ruby").hide();
     $("#react").hide();
+    $("#html").hide();
   });
   $("input#customRadio2").click(function(){
     $("#ruby").show();
     $("#javascript").hide();
     $("#react").hide();
+    $("#html").hide();
   });
   $("input#customRadio3").click(function(){
     $("#react").show();
     $("#javascript").hide();
     $("#ruby").hide();
+    $("#html").hide();
+  });
+  $("input#customRadio4").click(function(){
+    $("#html").show();
+    $("#javascript").hide();
+    $("#ruby").hide();
+    $("#react").hide();
   });
     console.log( "ready!" );
 });
