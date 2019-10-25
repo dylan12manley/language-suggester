879419ca7045d0d9850572b5b53021e78525dace
Dylan Manley <dylan.manley@gmail.com>
2019-10-25 11:14:13 -0700

    add functionality to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $( document ).ready(function() {
   $("input#customRadio1").click(function(){
     $("#javascript").show();
-  })
+    $("#ruby").hide();
+    $("#react").hide();
+  });
+  $("input#customRadio2").click(function(){
+    $("#ruby").show();
+    $("#javascript").hide();
+    $("#react").hide();
+  });
+  $("input#customRadio3").click(function(){
+    $("#react").show();
+    $("#javascript").hide();
+    $("#ruby").hide();
+  });
     console.log( "ready!" );
 });
